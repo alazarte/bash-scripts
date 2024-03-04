fbb586b17ce306572cd2cab4c364377e9faa026d
alazarte <inbox@alazarte.com>
2024-03-04 12:26:16 -0300

    rsync script for backup my home folder

---
+++ rsync.sh
@@ -0,0 +1,40 @@
+#!/bin/bash
+
+#rsync -aP --exclude-from=ignorelist /home/$USER/ ${REMOTE}:/home/$USER/
+
+LOCAL_FOLDER="/home/$USER/"
+ignorelist_filepath="ignorelist"
+read -p "local or remote? (l/r): " option
+
+case $option in
+	l)
+		tmpfile=$(mktemp)
+
+		ls /mnt/ | grep mnt_ > $tmpfile
+		cat -n $tmpfile
+		read -p "Which path?: " option
+		folder=$(sed -n "${option}p" "$tmpfile")
+
+		REMOTE_FOLDER="/mnt/$folder/home/"
+		if [ ! -d $REMOTE_FOLDER ]; then
+			echo "Folder doesn't exists: $REMOTE_FOLDER"
+			exit 1
+		fi
+		;;
+	*)
+		read -p "remote IP: " ip
+		REMOTE_FOLDER="${ip}:/home/$USER/"
+		;;
+esac
+
+command="rsync -aP --exclude-from=${ignorelist_filepath} $LOCAL_FOLDER $REMOTE_FOLDER"
+echo $command
+read -p "run command? (y/n): " option
+case $option in
+	y)
+		eval "${command}"
+		;;
+	*)
+		echo Ok
+		;;
+esac
